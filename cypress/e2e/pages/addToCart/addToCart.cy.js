6d52b83e010cd4fee19bf42c36e80cc5fb5122ca
Ahmed_Mujtaba <ahmedmujtaba1944@gmail.com>
2024-11-04 09:58:48 +0500

    writing more test script

--- cypress/e2e/pages/addToCart/addToCart.cy.js
+++ cypress/e2e/pages/addToCart/addToCart.cy.js
@@ -1,19 +1,120 @@
+
+
 describe("test suite for add to cart page", () => {
   beforeEach("visit url", () => {
     cy.visit("https://demowebshop.tricentis.com/");
   });
+
+  //test case 1
   it("test case 1: Verify adding a product to the cart from the product listing page", () => {
-    cy.get(".top-menu > :nth-child(5) > a").click();
+    cy.get(".top-menu > :nth-child(3)").click();
+    cy.get(":nth-child(2) > .sub-category-item > .title > a").click();
     cy.get(
-      ":nth-child(1) > .product-item > .details > .product-title > a"
+      ":nth-child(1) > .product-item > .details > .add-info > .buttons > .button-2"
     ).click();
-    cy.get("#add-to-wishlist-button-53").click();
     cy.get(".content").should(
       "contain",
-      "The product has been added to your wishlist"
+      "The product has been added to your shopping cart"
     );
-    cy.get(".wishlist-qty").should("be.visible");
-    cy.get(".ico-wishlist > .cart-label").click();
-    cy.get(".product > a").should("contain", "3rd Album");
+    cy.get(".ico-cart > .cart-label").click();
+    cy.get(".product-name").should("contain", "Smartphone");
+  });
+
+  //test case 2
+  it("Verify that the cart displays the correct product count afteradding multiple items", () => {
+    cy.get(".top-menu > :nth-child(6) > a").click();
+    cy.get(
+      ":nth-child(2) > .product-item > .details > .add-info > .buttons > .button-2"
+    ).click();
+    cy.get(".content").should(
+      "contain",
+      "The product has been added to your shopping cart"
+    );
+    cy.wait(2000);
+    cy.get(
+      ":nth-child(2) > .product-item > .details > .add-info > .buttons > .button-2"
+    ).click();
+    cy.get(".content").should(
+      "contain",
+      "The product has been added to your shopping cart"
+    );
+    cy.wait(2000);
+    cy.get(
+      ":nth-child(2) > .product-item > .details > .add-info > .buttons > .button-2"
+    ).click();
+    cy.get(".content").should(
+      "contain",
+      "The product has been added to your shopping cart"
+    );
+    cy.wait(2000);
+    cy.get(".cart-qty").should("contain", 3);
+  });
+
+  it("Verify that the remove item from the cart", () => {
+    cy.get(".top-menu > :nth-child(6) > a").click();
+    cy.get(
+      ":nth-child(2) > .product-item > .details > .add-info > .buttons > .button-2"
+    ).click();
+    cy.get(".content").should(
+      "contain",
+      "The product has been added to your shopping cart"
+    );
+    cy.wait(2000);
+    cy.get(
+      ":nth-child(2) > .product-item > .details > .add-info > .buttons > .button-2"
+    ).click();
+    cy.get(".content").should(
+      "contain",
+      "The product has been added to your shopping cart"
+    );
+    cy.wait(2000);
+    cy.get(
+      ":nth-child(2) > .product-item > .details > .add-info > .buttons > .button-2"
+    ).click();
+    cy.get(".content").should(
+      "contain",
+      "The product has been added to your shopping cart"
+    );
+    cy.wait(2000);
+
+    cy.get(".ico-cart > .cart-label").click();
+    cy.get(".remove-from-cart > input").check();
+    cy.get(".update-cart-button").click();
+
+    cy.contains("Your Shopping Cart is empty!").should("exist");
+    // Your Shopping Cart is empty!
+  });
+
+
+  it.only("Verify updating item quantity in the cart", () => {
+    cy.get(".top-menu > :nth-child(1) > a").click();
+
+    // adding first item to cart
+    cy.get(':nth-child(1) > .product-item > .details > .add-info > .buttons > .button-2').click();
+    cy.get(".content").should("contain", "The product has been added to your shopping cart");
+    cy.wait(2000);
+    cy.get(".cart-qty").should("contain", 1);
+
+
+    cy.get(".ico-cart > .cart-label").click();
+    cy.contains("Shopping cart").should('exist');
+    cy.url().should("eq", "https://demowebshop.tricentis.com/cart");
+    cy.get(".product-name").should("contain", "Computing and Internet");
+
+
+
+    // going back to books pdp
+    cy.get(".top-menu > :nth-child(1) > a").click();
+    
+    // adding second item to cart
+    cy.get(':nth-child(3) > .product-item > .details > .add-info > .buttons > .button-2').click();
+    cy.get(".content").should("contain", "The product has been added to your shopping cart");
+    cy.wait(2000);
+    cy.get(".cart-qty").should("contain", 2);
+
+    cy.get(".ico-cart > .cart-label").click();
+    cy.contains("Shopping cart").should("exist");
+    cy.url().should("eq", "https://demowebshop.tricentis.com/cart");
+    cy.get(".product-name").should("contain", "Fiction");
   });
 });
---
+++ cypress/e2e/pages/checkout/checkout.cy.js
@@ -0,0 +1,52 @@
+describe("test suite for checkout page", () => {
+  beforeEach("visit the url", () => {
+    cy.visit("https://demowebshop.tricentis.com/");
+  });
+  it("check out without login using COD", () => {
+    cy.get(".top-menu > :nth-child(4) > a").click();
+    cy.get(
+      ":nth-child(1) > .product-item > .details > .add-info > .buttons > .button-2"
+    ).click();
+    cy.get("#add-to-cart-button-5").click();
+    cy.contains("The product has been added to your shopping cart").should(
+      "exist"
+    );
+    cy.get("#topcartlink > .ico-cart").trigger("mouseover");
+    cy.get(".cart-button").should("be.visible");
+    cy.get(".cart-button").click();
+    cy.get("#termsofservice").check();
+    cy.get("#checkout").click();
+    cy.contains("Checkout as a guest or register").should("exist");
+
+    cy.get(".checkout-as-guest-button").click();
+    cy.get("#BillingNewAddress_FirstName").type("test");
+    cy.get("#BillingNewAddress_LastName").type("ch");
+    cy.get("#BillingNewAddress_Email").type("test-ahmed@yopmail.com");
+
+    // cy.get('select').select('Afghanistan');
+    cy.get("select").select("Afghanistan").should("have.value", "86");
+
+    cy.get("#BillingNewAddress_City").type("test city");
+    cy.get("#BillingNewAddress_Address1").type("test address 1");
+    cy.get("#BillingNewAddress_ZipPostalCode").type("2323");
+    cy.get("#BillingNewAddress_PhoneNumber").type("123456789");
+
+    cy.get("#billing-buttons-container > .button-1").click();
+    cy.get("#shipping-buttons-container > .button-1").click();
+    cy.get('[type="radio"]').first().check();
+    cy.get("#shipping-method-buttons-container > .button-1").click();
+    cy.get('[type="radio"]').first().check();
+    cy.get("#payment-method-buttons-container > .button-1").click();
+
+    // payment page
+    cy.contains("You will pay by COD").should("exist");
+    cy.get("#payment-info-buttons-container > .button-1").click();
+
+    // confirmation page
+    cy.get("#confirm-order-buttons-container > .button-1").click();
+
+    // thanku page
+    cy.contains("Thank you").should("exist");
+    cy.contains("Your order has been successfully processed!").should("exist");
+  });
+});
